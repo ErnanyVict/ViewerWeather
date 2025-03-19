ce17a546477ff50d2e03f1f15672fe50d4042bc6
ErnayVict <ernany12victor@gmail.com>
2025-03-19 13:08:43 -0300

    Program

--- Program.py
+++ Program.py
@@ -5,9 +5,9 @@ def fecth_data(endpoints = None):
     response = requests.get(f"https://api.hgbrasil.com/weather?woeid=455912")
     return response.json()
 
-dados = fecth_data()
+datas = fecth_data()
 
 '''print(json.dump(dados, indent=4))
 '''
-# print(dados)
-print(dados["results"])
+# print(datas)
+print(datas["results"])
